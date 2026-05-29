84cc02aa3c04a376854ef027117557c7e0b060b9
Scanzybd <scanzybd@gmail.com>
2026-05-29 15:47:42 +0600

    2nd commit

--- src/config/db.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import { env } from "./env";
-
-export async function connectDB(): Promise<void> {
-  await mongoose.connect(env.MONGODB_URI);
-}
--- src/constants/roles.ts
+++
@@ -1,8 +0,0 @@
-export const ROLES = {
-  USER: "user",
-  OWNER: "owner",
-  ADMIN: "admin",
-  STAFF: "staff",
-} as const;
-
-export type Role = (typeof ROLES)[keyof typeof ROLES];
